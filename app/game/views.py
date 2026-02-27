188f1b175646582379731f58e9714ce35f5e2da0
Taubin <ty.aubin@gmail.com>
2026-02-27 14:47:44 -0500

    display registration errors

--- app/game/views.py
+++ app/game/views.py
@@ -7,3 +7,19 @@ def home(request):
 @login_required
 def dashboard(request):
     return render(request, "dashboard.html")
+
+from django.shortcuts import redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("dashboard")
+    else:
+        form = RegisterForm()
+
+    return render(request, "register.html", {"form": form})
--- app/secure_arcade/urls.py
+++ app/secure_arcade/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('register/', views.register, name='register'),
 ]
