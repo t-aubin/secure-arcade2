3604d4194d83c9ec8e043ae66756958367eafc34
Taubin <ty.aubin@gmail.com>
2026-02-27 15:03:01 -0500

    added user model for game stats

--- app/game/models.py
+++ app/game/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    wins = models.IntegerField(default=0)
+    losses = models.IntegerField(default=0)
+    games_played = models.IntegerField(default=0)
+    elo_rating = models.IntegerField(default=1000)
+
+    def __str__(self):
+        return self.username
